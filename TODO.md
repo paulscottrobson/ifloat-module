48ef39916169ceca62cfa9f107ea451de0f5ab12
Paul Robson <paul@robsons.org.uk>
2025-03-20 18:03:07 +0000

    Single file version works

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 # TODO List
 
 - Outer call (needs to reset internal stack and the string size and data)
-- Make one file, rewrite builder.py
 - Write a better test suite, though polynomials are pretty good.
 
--- source/scripts/builder.py
+++ source/scripts/builder.py
@@ -11,3 +11,12 @@
 
 import os,sys,re
 
+for s in open("float.asm").readlines():
+    if s.find(".include") >= 0:
+        m = re.match('^\\s*\\.include\\s*\\"(.*?)\\"',s)
+        assert m is not None,"Bad include "+s
+        for s1 in open(m.group(1)).readlines():
+            assert s1.find(".include") < 0,s + "bad line"
+            print(s1.rstrip())
+    else:
+        print(s.rstrip())
